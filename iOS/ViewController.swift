14ed2d07b792b7b2bb91c6bd40b5bc90a7f58a1d
shubham-jitiya <shubham.j@simformsolutions.com>
2023-04-21 14:59:04 +0530

    - completed basics of labels, buttons, text-field & text view

--- iOS/ViewController.swift
+++ iOS/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var lblWelcome: UILabel!
+    @IBOutlet weak var lblName: CustomTextField!
+    
+    //MARK: - View life cycles
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblWelcome.text = "View did load"
+        btnOne.layer.cornerRadius = 16
+        btnOne.clipsToBounds = true
+        btnOne.setTitle("BtnOne", for: .normal)
+        lblName.layer.borderColor = UIColor.blue.cgColor
+        lblName.layer.borderWidth = 1
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View did disappear")
+    }
+    
+    //MARK: - IB Action
+    @IBAction func btnOne(_ sender: UIButton) {
+        print("Button one pressed")
+    }
 }
 
